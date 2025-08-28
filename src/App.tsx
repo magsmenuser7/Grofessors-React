cfda1e5f27a2553426f3734245570b53943fe3f8
magsmenuser7 <magsmenuser7@gmail.com>
2025-08-28 18:42:42 +0530

    app.ttsx file updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import Header from './components/layout/Header';
