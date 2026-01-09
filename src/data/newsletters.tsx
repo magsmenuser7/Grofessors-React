04051a8a265184d898cec546e8a0b63da56f744d
magsmenuser7 <magsmenuser7@gmail.com>
2026-01-09 20:08:30 +0530

    changes complted

--- src/data/newsletters.tsx
+++ src/data/newsletters.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import nirvapdf from "../components/assets/logos/newsletters/NIRVA_Brand_Analysis.pdf"
+import brandauditsystem from "../components/assets/logos/newsletters/brand-audit-system.pdf"
 
 
 export const newsletters = [
@@ -7,6 +8,12 @@ export const newsletters = [
     slug: "nirva-brand-analysis",
     title: "NIRVA Brand Analysis",
     pdf: nirvapdf
+  },
+
+    {
+    slug: "brand-audit-system",
+    title: "Brand Audit System",
+    pdf: brandauditsystem
   },
  
 ];
