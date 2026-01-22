4de5ae5a754f0f98b9b73ccb0a033552f8492f79
magsmenuser7 <magsmenuser7@gmail.com>
2026-01-22 12:29:29 +0530

    newsletter added

--- src/data/newsletters.tsx
+++ src/data/newsletters.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import nirvapdf from "../components/assets/logos/newsletters/NIRVA_Brand_Analysis.pdf"
 import brandauditsystem from "../components/assets/logos/newsletters/brand-audit-system.pdf"
+import scorecarebrandnameproposal from "../components/assets/logos/newsletters/Scorecare-Brand-name-Proposal.pdf"
+
 
 
 export const newsletters = [
@@ -14,6 +16,11 @@ export const newsletters = [
     slug: "brand-audit-system",
     title: "Brand Audit System",
     pdf: brandauditsystem
+  },
+  {
+    slug: "scorecare-brand-name-proposal",
+    title: "Scorecare Brand name Proposal",
+    pdf: scorecarebrandnameproposal
   },
  
 ];
